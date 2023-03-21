271156e7449ca8a0264436cd950ac0fa4ec15685
Luis Developer <luismq7890@gmail.com>
2023-03-20 21:38:26 -0500

    Se inicia el proyecto de la app movil GosueSportApp

---
+++ app/src/main/java/com/g4/dev/gosuesprortsapp/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.g4.dev.gosuesprortsapp.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
